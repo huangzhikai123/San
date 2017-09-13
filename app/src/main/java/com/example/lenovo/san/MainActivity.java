7fbde5c76ae4680028583aecc00f4d900c89b3a1
huangzhikai123 <1457044032@qq.com>
2017-09-13 10:38:06 +0800

    Initial commit

---
+++ app/src/main/java/com/example/lenovo/san/MainActivity.java
@@ -0,0 +1,46 @@
+package com.example.lenovo.san;
+
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button button = (Button) findViewById(R.id.button2);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (ActivityCompat.checkSelfPermission(MainActivity.this, Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED) {
+                    Toast.makeText(MainActivity.this, "缺少打电话权限", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                EditText editText = (EditText)findViewById(R.id.textView);
+
+                String phonenumber=editText.getText().toString() ;  // , = pauses
+                String encodedPhonenumber = null;
+                try {
+                    encodedPhonenumber = URLEncoder.encode(phonenumber, "UTF-8");
+                } catch (
+                        UnsupportedEncodingException e) {
+                    e.printStackTrace();
+                }
+                startActivity(new Intent(Intent.ACTION_CALL, Uri.parse("tel:" + encodedPhonenumber)));
+            }
+
+        });
+    }
+}
